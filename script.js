4766df9a113b8e293fcc40b4da05927a49c95d39
Glitch (glitch-hello-website) <none>
2022-04-13 20:45:11 +0000

    🕌🌗 Checkpoint ./index.html:43481304/379 ./script.js:43481304/1175 ./style.css:43481304/246

---
+++ script.js
@@ -0,0 +1,42 @@
+//user input grid size
+var size = window.prompt("Enter Number for Grid Size: ");
+
+//function that loads the game
+function startgame() {
+    var display = document.getElementById("grid");
+    var grid = creategrid();
+    display.appendChild(grid);  
+}
+
+//creating grid
+function creategrid() {
+    var grid = document.createElement("grid");
+    grid.setAttribute("id", "game_grid");
+    for (var i = 0; i < size; i++) {
+        var tr = "tr_" + i;
+        var row = gridrows(tr);
+        for (var j = 0; j < size; j++) {
+            var td = "td_" + i + "_" + j;
+            var col = gridcolumns(td);
+            row.appendChild(col);
+        }
+        grid.appendChild(row);
+    }
+    return grid;
+}
+
+function gridrows(name) {
+    var rowattribute = document.createElement("tr");
+    rowattribute.setAttribute("id", name);
+    rowattribute.setAttribute("onclick","")
+    return rowattribute;
+}
+
+function gridcolumns(name) {
+    var colattribute = document.createElement("td");
+    colattribute.setAttribute("id", name);
+    colattribute.style.height = (500 / size) + "px";
+    colattribute.style.width = (500 / size) + "px";
+    return colattribute;
+}
+
